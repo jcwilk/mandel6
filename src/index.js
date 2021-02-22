7a7ade4f9e66ca971c41ee76efbcb810ee0c3add
John Wilkinson <me@jcwilk.com>
2021-02-21 21:06:18 -0800

    Add support for asset modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Buttons from './buttons.png';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    // Add the image to our existing div.
+    const buttons = new Image();
+    buttons.src = Buttons;
+
+    element.appendChild(buttons);
 
     return element;
 }

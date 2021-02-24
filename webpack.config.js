bac538bbf7b2c53dc807e02ac8aaba498a4eec5c
John Wilkinson <me@jcwilk.com>
2021-02-24 01:14:08 -0800

    Dynamic index and better build cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Mandel4',
+            minify: {
+                collapseWhitespace: false
+            }
+        }),
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'docs'),
